99592e2e7f2ad44a23dffc90a71c8e6bba0af6e3
AndKey1703 <162692322+AndKey1703@users.noreply.github.com>
2024-03-12 13:33:01 +0200

    Update index.php

--- Stage 1/ex 1/index.php
+++ Stage 1/ex 1/index.php
@@ -8,10 +8,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         die("Некорректный email адрес");
     }
     $file_path = '';
+    $allowedFormats = ['image/jpeg', 'image/png'];
     if ($_FILES['file']['error'] === 0) {
         $file_name = $_FILES['file']['name'];
         $file_path = 'uploads/' . $file_name;
         move_uploaded_file($_FILES['file']['tmp_name'], $file_path);
+        if(in_array($fileType, $allowedFormats)) 
+        {
+
+        } 
+        else 
+        {
+        echo 'Допустимы только файлы в формате JPEG и PNG';
+        }
     }
     $data = "Имя: $name\nE-mail: $email\nСообщение: $message\nФайл: $file_path\n\n";
     $file = fopen("feedback.txt", "a");
